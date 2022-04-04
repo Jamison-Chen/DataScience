b9af2ab24441e30aaab4300338941882f404cb87
Jamison <106208004@g.nccu.edu.tw>
2022-04-04 21:41:48 +0800

    implement mini batch on tlfcnn

--- myToolBox/models.py
+++ myToolBox/models.py
@@ -1,4 +1,5 @@
 import abc
+import random
 import numpy as np
 from scipy.stats import multivariate_normal
 from copy import deepcopy
@@ -94,13 +95,12 @@ class GenerativeModel(Model):
 
 
 class DichotomousLogistic(Model):
-    def __init__(self, learningRate, numOfLoop):
+    def __init__(self):
         self.__w = None
         self.__b = 0
         self.__classes = []
-        self.__lr = learningRate
-        self.numOfLoop = numOfLoop
         self.sigmoid = Sigmoid()
+        self.epoch = None
         self.lossHistory = []
 
     def __crossEntropy(self, y, y_pred):
@@ -108,15 +108,16 @@ class DichotomousLogistic(Model):
             np.where(y == self.__classes[0], np.log(y_pred), np.log(1 - y_pred))
         )
 
-    def fit(self, x, y):
+    def fit(self, x, y, lr, epoch):
         self.__w = np.random.uniform(-1, 1, (x.shape[1],))
         self.__b = np.random.uniform(-1, 1)
         self.__classes = np.unique(y)
         self.lossHistory = []
+        self.epoch = epoch
         y_squeezed = deepcopy(y).squeeze()
         y_hat = np.where(y_squeezed == self.__classes[0], 1, 0)
 
-        for _ in range(self.numOfLoop):
+        for _ in range(epoch):
             y_pred = self.sigmoid.forward(x @ self.__w + self.__b)
 
             # Store loss (for ploting)
@@ -125,8 +126,8 @@ class DichotomousLogistic(Model):
             )
 
             # update w and b using gradient descent
-            self.__w -= np.sum((y_pred - y_hat)[:, np.newaxis] * x, axis=0) * self.__lr
-            self.__b -= np.sum((y_pred - y_hat)[:, np.newaxis], axis=0) * self.__lr
+            self.__w -= np.sum((y_pred - y_hat)[:, np.newaxis] * x, axis=0) * lr
+            self.__b -= np.sum((y_pred - y_hat)[:, np.newaxis], axis=0) * lr
 
     def predict(self, x):
         y_pred = self.sigmoid.forward(x @ self.__w + self.__b)
@@ -136,21 +137,21 @@ class DichotomousLogistic(Model):
 
 
 class LogisticRegression(Model):
-    def __init__(self, learningRate=0.005, numOfLoop=200):
+    def __init__(self):
         self.__w = None
         self.__b = None
         self.__classes = {}
-        self.__lr = learningRate
-        self.numOfLoop = numOfLoop
         self.softmax = Softmax()
         self.crossEntropy = CrossEntropy()
+        self.epoch = None
         self.lossHistory = []
 
-    def fit(self, x_train, y_train):
+    def fit(self, x_train, y_train, lr=0.005, epoch=200):
         uniqVals = np.unique(y_train)
         self.__w = np.random.uniform(-1, 1, (x_train.shape[1], len(uniqVals)))
         self.__b = np.random.uniform(-1, 1, (len(uniqVals),))
         self.lossHistory = []
+        self.epoch = epoch
         y = deepcopy(y_train)
 
         # Encode class names into numerical numbers
@@ -162,7 +163,7 @@ class LogisticRegression(Model):
         y = np.where(np.arange(len(uniqVals)) == y, 1, 0)
 
         # Training
-        for _ in range(self.numOfLoop):
+        for _ in range(epoch):
             y_pred = self.softmax.forward(x_train @ self.__w + self.__b)
 
             # Store loss (for ploting)
@@ -183,8 +184,8 @@ class LogisticRegression(Model):
             )
 
             # Update w and b using gradient descent
-            self.__w -= w_gradient * self.__lr
-            self.__b -= b_gradient * self.__lr
+            self.__w -= w_gradient * lr
+            self.__b -= b_gradient * lr
 
     def predict(self, x):
         y_pred = self.softmax.forward(x @ self.__w + self.__b)
@@ -196,13 +197,12 @@ class LogisticRegression(Model):
 
 
 class TL_FC_NN(Model):
-    def __init__(self, hiddenLayerNodeNumber, learningRate, numOfLoop):
+    def __init__(self, hiddenLayerNodeNumber):
         self.__w1 = None
         self.__w2 = None
         self.__classes = {}
         self.__numOfNode = hiddenLayerNodeNumber
-        self.__lr = learningRate
-        self.numOfLoop = numOfLoop
+        self.epoch = None
         self.lossHistory = []
 
         # `h` denotes the activation function of the hidden layer
@@ -242,11 +242,18 @@ class TL_FC_NN(Model):
                 "Please provide an activation function instead of {}".format(type(l))
             )
 
-    def fit(self, x_train, y_train):
+    def fit(self, x_train, y_train, lr, batch_size=None, epoch=1000):
+        # `batch_size` defaults to full-batch
+        if not batch_size:
+            batch_size = x_train.shape[0]
+
+        # Initialize weights
         uniqVals = np.unique(y_train)
         self.__w1 = np.random.uniform(-1, 1, (x_train.shape[1], self.__numOfNode))
         self.__w2 = np.random.uniform(-1, 1, (self.__numOfNode, len(uniqVals)))
         self.lossHistory = []
+        self.epoch = epoch
+        x = deepcopy(x_train)
         y = deepcopy(y_train)
 
         # Encode each class name to numerical numbers
@@ -257,28 +264,43 @@ class TL_FC_NN(Model):
         # Transform the true answers into vectors
         y = np.where(np.arange(len(uniqVals)) == y, 1, 0)
 
+        # Slice the batches
+        batches = []
+        temp = list(range(x_train.shape[0]))
+        random.shuffle(temp)
+        while len(temp) > 0:
+            argsliced = temp[:batch_size]
+            batches.append({"x": x[argsliced, :], "y": y[argsliced, :]})
+            temp = temp[batch_size:]
+
         # Training
-        for _ in range(self.numOfLoop):
-            # Forward
-            h = self.__h.forward(x_train @ self.__w1)  # (n, k)
-            if isinstance(self.__l, CrossEntropy):
-                y_pred = self.softmax.forward(h @ self.__w2)  # (n, c)
-            else:
-                y_pred = h @ self.__w2  # (n, c)
+        for _ in range(epoch):
+            batchLoss = 0
+            for batch in batches:
+                # Forward
+                h = self.__h.forward(batch["x"] @ self.__w1)  # (n, k)
+                if isinstance(self.__l, CrossEntropy):
+                    y_pred = self.softmax.forward(h @ self.__w2)  # (n, c)
+                else:
+                    y_pred = h @ self.__w2  # (n, c)
 
+                # Accumulate batch loss
+                batchLoss += self.__l.forward(batch["y"], y_pred) / y_pred.shape[0]
+
+                # Backward
+                if isinstance(self.__l, CrossEntropy):
+                    w2_gradient = h.T @ (self.softmax.backward() * self.__l.backward())
+                else:
+                    w2_gradient = h.T @ self.__l.backward()  # (k, c)
+
+                w1_gradient = batch["x"].T @ (
+                    self.__h.backward() * (self.__l.backward() @ self.__w2.T)
+                )  # (m, k)
+
+                self.__w2 -= w2_gradient * lr  # (k, c)
+                self.__w1 -= w1_gradient * lr  # (m, k)
             # Store loss (for ploting)
-            self.lossHistory.append(self.__l.forward(y, y_pred) / y_pred.shape[0])
-
-            # Backward
-            if isinstance(self.__l, CrossEntropy):
-                w2_gradient = h.T @ (self.softmax.backward() * self.__l.backward())
-            else:
-                w2_gradient = h.T @ self.__l.backward()  # (k, c)
-            w1_gradient = x_train.T @ (
-                self.__h.backward() * (self.__l.backward() @ self.__w2.T)
-            )  # (m, k)
-            self.__w2 -= w2_gradient * self.__lr  # (k, c)
-            self.__w1 -= w1_gradient * self.__lr  # (m, k)
+            self.lossHistory.append(batchLoss)
 
     def predict(self, x):
         h = self.__h.forward(x @ self.__w1)  # (n, k)
